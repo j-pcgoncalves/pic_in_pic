dbe79d5cc19915d1e6b90c209cbe6c9d145cedf3
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-11-11 18:37:15 +0000

    use screen capture API to share, click btn to PIP

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement = document.getElementById("video");
+const btn = document.getElementById("btn");
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => videoElement.play();
+    } catch (err) {
+        // Catch Error here
+        console.log("whoops, error here: ", err);
+    }
+};
+
+btn.addEventListener("click", async () => {
+    // Disable Button
+    btn.disabled = true;
+
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+
+    // Reset Button
+    btn.disabled = false;
+});
+
+// On Load
+selectMediaStream();
